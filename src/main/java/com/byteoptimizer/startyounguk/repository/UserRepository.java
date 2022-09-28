0b3dbb380657e59821403ac94d128402f2bfb1f0
shubhamshirsat <shubhamshirsat00@gmail.com>
2022-09-28 19:47:52 +0530

    Added packages

---
+++ src/main/java/com/byteoptimizer/startyounguk/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.byteoptimizer.startyounguk.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.byteoptimizer.startyounguk.model.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
