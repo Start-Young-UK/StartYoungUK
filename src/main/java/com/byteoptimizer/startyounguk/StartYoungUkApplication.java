3213a3c7aa3649c7face5b8856f0f242198e8dcf
shubhamshirsat <shubhamshirsat00@gmail.com>
2022-09-28 17:08:46 +0530

    Initial Commit

---
+++ src/main/java/com/byteoptimizer/startyounguk/StartYoungUkApplication.java
@@ -0,0 +1,13 @@
+package com.byteoptimizer.startyounguk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartYoungUkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartYoungUkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byteoptimizer/startyounguk/StartYoungUkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteoptimizer.startyounguk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartYoungUkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
