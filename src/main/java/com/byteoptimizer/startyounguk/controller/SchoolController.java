5121e842adb9556b0bce5ab0128fb2aa849e5cbe
shubhamshirsat <shubhamshirsat00@gmail.com>
2022-09-29 13:37:01 +0530

    test

--- src/main/java/com/byteoptimizer/startyounguk/controller/SchoolController.java
+++ src/main/java/com/byteoptimizer/startyounguk/controller/SchoolController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.byteoptimizer.startyounguk.model.School;
@@ -45,7 +47,7 @@ public class SchoolController {
 //		
 //	}
 	
-	@PostMapping("/school")
+	@RequestMapping(method =RequestMethod.POST, value="/school", headers = "Accept=application/json, Content-type=application/json")
 	public School createSchool(@RequestBody School school) {
 		
 		UserRoles userRoles = new UserRoles(2, "SCHOOL");
@@ -62,7 +64,7 @@ public class SchoolController {
 	}
 
 	
-	@PostMapping("/test")
+	@RequestMapping(method =RequestMethod.POST, value="/test", headers = "Accept=application/json, Content-type=application/json")
 	public Map<String, Object> test(@RequestBody Map<String, Object> payload) {
 		return payload;
 	}
